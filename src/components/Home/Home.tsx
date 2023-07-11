573ee95fbe989000df19ae78cc359717c59490b1
Mandeep Sai <bandimandeep7@gmail.com>
2023-07-11 14:36:59 +0200

    refactor: use react query api to fetch data and display loading state

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,48 +1,52 @@
-import { useEffect, useState } from "react";
-import { ArtObject } from "../../types";
-import fetchAll from "../../utilities/fetchData";
+import { useFetchArts } from "../../utilities/fetchData";
 import "./Home.css";
 
 const Home = () => {
-  const [artObjects, setArtObjects] = useState<ArtObject[]>([]);
-  useEffect(() => {
-    fetchAll()
-      .then((data) => setArtObjects(data))
-      .catch((err) => console.error(err));
-  }, []);
+  const { data, error, isError, isLoading } = useFetchArts();
+
+  if (isLoading) {
+    return (
+      <div className="loading">
+        <div className="spinner-border" role="status"></div>
+      </div>
+    );
+  }
+
+  if (isError) {
+    return (
+      <div className="error">
+        <h1>Error while fetching Data.</h1>
+        <h2>Please visit after some time.</h2>
+      </div>
+    );
+  }
+
   return (
     <div className="home">
       <h1>All artwork</h1>
       <div className="container-fluid" style={{ marginTop: "32px" }}>
         <div className="row ">
-          {artObjects.length > 0 ? (
-            <>
-              {artObjects.map((artObject, index) => {
-                return (
-                  <>
-                    <div
-                      key={index}
-                      className="col col-12 col-sm-6 col-md-6 col-lg-4 "
-                    >
-                      <div className="art">
-                        <img
-                          src={artObject.headerImage.url}
-                          alt="art image"
-                          className="art_image"
-                        />
-                        <p className="art_first_maker">
-                          {artObject.principalOrFirstMaker}
-                        </p>
-                        <h3 className="art_title">{artObject.longTitle}</h3>
-                      </div>
-                    </div>
-                  </>
-                );
-              })}
-            </>
-          ) : (
-            <h1>No Data</h1>
-          )}
+          {data !== undefined &&
+            data.map((artObject, index) => {
+              return (
+                <div
+                  key={index}
+                  className="col col-12 col-sm-6 col-md-6 col-lg-4 "
+                >
+                  <div className="art">
+                    <img
+                      src={artObject.headerImage.url}
+                      alt="art image"
+                      className="art_image"
+                    />
+                    <p className="art_first_maker">
+                      {artObject.principalOrFirstMaker}
+                    </p>
+                    <h3 className="art_title">{artObject.title}</h3>
+                  </div>
+                </div>
+              );
+            })}
         </div>
       </div>
     </div>
--- src/utilities/fetchData.ts
+++ src/utilities/fetchData.ts
@@ -1,16 +1,22 @@
-import { ArtObject, Arts } from "../types";
+import { Arts } from "../types";
+import { useQuery } from "react-query";
 
-const fetchAll = async (): Promise<ArtObject[]> => {
-  try {
+export const useFetchArts = () => {
+  return useQuery("arts", async () => {
     const response = await fetch(
       "https://www.rijksmuseum.nl/api/nl/collection?key=2esrTh6M&ps=9"
     );
     const parsedResponse: Arts = (await response.json()) as Arts;
     return parsedResponse.artObjects;
-  } catch (error) {
-    console.log(error);
-    return Promise.reject(error);
-  }
+  });
+  //   try {
+  //     const response = await fetch(
+  //       "https://www.rijksmuseum.nl/api/nl/collection?key=2esrTh6M&ps=9"
+  //     );
+  //     const parsedResponse: Arts = (await response.json()) as Arts;
+  //     return parsedResponse.artObjects;
+  //   } catch (error) {
+  //     console.log(error);
+  //     return Promise.reject(error);
+  //   }
 };
-
-export default fetchAll;
